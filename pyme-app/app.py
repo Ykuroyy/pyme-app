ef25233c24fb4142ceee75be7389583e16804a98
Ykuroyy <yykm666@gmail.com>
2025-07-07 11:08:28 +0900

    ポート設定

--- pyme-app/app.py
+++ pyme-app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -278,4 +279,5 @@ def tool_detail(tool_id):
     return render_template('detail.html', tool=tool)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False) 
